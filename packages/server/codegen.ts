01632764832dd23313abf73d0b05a874fc90577d
Thomas Kowalski <Thomas.Kowalski@exxeta.com>
2023-03-17 13:10:56 +0100

    changes

--- packages/server/codegen.ts
+++ packages/server/codegen.ts
@@ -1,22 +1,31 @@
-import { preset as serverPreset } from "@eddeee888/gcg-typescript-resolver-files"
+import { preset } from "@eddeee888/gcg-typescript-resolver-files"
 import type { CodegenConfig } from "@graphql-codegen/cli"
 
 const config: CodegenConfig = {
-  overwrite: true,
-  schema: "src/**/schema.graphql",
+  schema: "**/schema.graphql",
   generates: {
     "src/schema": {
-      preset: serverPreset,
-      config: {
-        contextType: "src/db/models#GqlContext",
-        mappers: {
-          TextQuestion: "src/db/models#TextQuestionDb",
-          SelectQuestion: "src/db/models#SelectQuestionDb",
-          Questionnaire: "src/db/models#QuestionnaireDb",
-        },
-      },
+      preset,
     },
   },
 }
+
+// const config: CodegenConfig = {
+//   overwrite: true,
+//   schema: "src/**/schema.graphql",
+//   generates: {
+//     "src/schema": {
+//       preset: serverPreset,
+//       // config: {
+//       //   contextType: "src/db/models#GqlContext",
+//       //   mappers: {
+//       //     TextQuestion: "src/db/models#TextQuestionDb",
+//       //     SelectQuestion: "src/db/models#SelectQuestionDb",
+//       //     Questionnaire: "src/db/models#QuestionnaireDb",
+//       //   },
+//       // },
+//     },
+//   },
+// }
 
 export default config
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,14 @@
 import { renderGraphiQL } from "@graphql-yoga/render-graphiql"
-import { createYoga } from "graphql-yoga"
+import { createSchema, createYoga } from "graphql-yoga"
 import { createServer } from "node:http"
 import { dbCtx } from "./db/fake-db"
 import { GqlContext } from "./db/models"
-import { schema } from "./schema/schema"
+import { resolvers } from "./schema/resolvers.generated"
+import { typeDefs } from "./schema/typeDefs.generated"
 
 function main() {
   const context: GqlContext = { db: dbCtx }
+  const schema = createSchema<GqlContext>({ typeDefs, resolvers })
   const yoga = createYoga({ schema, context, renderGraphiQL })
   const server = createServer(yoga)
   server.listen(4000, () => {
