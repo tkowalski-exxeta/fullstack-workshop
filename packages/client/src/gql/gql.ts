ccd9323cfefcee2b9e1fe09d0854de532737f6d2
Timothee Glörfeld <timothee.gloerfeld@exxeta.com>
2023-03-17 13:40:31 +0000

    generated gql

---
+++ packages/client/src/gql/gql.ts
@@ -0,0 +1,52 @@
+/* eslint-disable */
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ */
+const documents = {
+    "\n  mutation CreateForm {\n    createForm(form: { title: \"form-1\" }) {\n      _id\n      title\n    }\n  }\n": types.CreateFormDocument,
+    "\n  mutation CreateQuestion($formId: ID!) {\n    q1: createQuestion(\n      formId: $formId\n      question: {\n        select: {\n          question: \"Do you like GraphQL?\"\n          options: [\"yes\", \"no\"]\n          multiSelect: false\n        }\n      }\n    ) {\n      __typename\n    }\n    q2: createQuestion(\n      formId: $formId\n      question: { text: { question: \"What do you like about GraphQL?\" } }\n    ) {\n      __typename\n    }\n  }\n": types.CreateQuestionDocument,
+    "\n  query GetQuestionnaire($formId: ID!) {\n    questionnaire(id: $formId) {\n      _id\n      title\n      questions {\n        __typename\n        ... on TextQuestion {\n          _id\n          question\n        }\n        ... on SelectQuestion {\n          _id\n          question\n          options\n          multiSelect\n        }\n      }\n    }\n  }\n": types.GetQuestionnaireDocument,
+};
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ *
+ *
+ * @example
+ * ```ts
+ * const query = graphql(`query GetUser($id: ID!) { user(id: $id) { name } }`);
+ * ```
+ *
+ * The query argument is unknown!
+ * Please regenerate the types.
+ */
+export function graphql(source: string): unknown;
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  mutation CreateForm {\n    createForm(form: { title: \"form-1\" }) {\n      _id\n      title\n    }\n  }\n"): (typeof documents)["\n  mutation CreateForm {\n    createForm(form: { title: \"form-1\" }) {\n      _id\n      title\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  mutation CreateQuestion($formId: ID!) {\n    q1: createQuestion(\n      formId: $formId\n      question: {\n        select: {\n          question: \"Do you like GraphQL?\"\n          options: [\"yes\", \"no\"]\n          multiSelect: false\n        }\n      }\n    ) {\n      __typename\n    }\n    q2: createQuestion(\n      formId: $formId\n      question: { text: { question: \"What do you like about GraphQL?\" } }\n    ) {\n      __typename\n    }\n  }\n"): (typeof documents)["\n  mutation CreateQuestion($formId: ID!) {\n    q1: createQuestion(\n      formId: $formId\n      question: {\n        select: {\n          question: \"Do you like GraphQL?\"\n          options: [\"yes\", \"no\"]\n          multiSelect: false\n        }\n      }\n    ) {\n      __typename\n    }\n    q2: createQuestion(\n      formId: $formId\n      question: { text: { question: \"What do you like about GraphQL?\" } }\n    ) {\n      __typename\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  query GetQuestionnaire($formId: ID!) {\n    questionnaire(id: $formId) {\n      _id\n      title\n      questions {\n        __typename\n        ... on TextQuestion {\n          _id\n          question\n        }\n        ... on SelectQuestion {\n          _id\n          question\n          options\n          multiSelect\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query GetQuestionnaire($formId: ID!) {\n    questionnaire(id: $formId) {\n      _id\n      title\n      questions {\n        __typename\n        ... on TextQuestion {\n          _id\n          question\n        }\n        ... on SelectQuestion {\n          _id\n          question\n          options\n          multiSelect\n        }\n      }\n    }\n  }\n"];
+
+export function graphql(source: string) {
+  return (documents as any)[source] ?? {};
+}
+
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
---
+++ packages/client/src/gql/graphql.ts
@@ -0,0 +1,111 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createForm?: Maybe<Questionnaire>;
+  createQuestion?: Maybe<Question>;
+};
+
+
+export type MutationCreateFormArgs = {
+  form: QuestionnaireInput;
+};
+
+
+export type MutationCreateQuestionArgs = {
+  formId: Scalars['ID'];
+  question: QuestionInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  questionnaire?: Maybe<Questionnaire>;
+};
+
+
+export type QueryQuestionnaireArgs = {
+  id: Scalars['ID'];
+};
+
+export type Question = {
+  _id: Scalars['ID'];
+  question: Scalars['String'];
+};
+
+export type QuestionInput = {
+  select?: InputMaybe<SelectQuestionInput>;
+  text?: InputMaybe<TextQuestionInput>;
+};
+
+export type Questionnaire = {
+  __typename?: 'Questionnaire';
+  _id: Scalars['ID'];
+  questions: Array<Question>;
+  title: Scalars['String'];
+};
+
+export type QuestionnaireInput = {
+  title: Scalars['String'];
+};
+
+export type SelectQuestion = Question & {
+  __typename?: 'SelectQuestion';
+  _id: Scalars['ID'];
+  multiSelect: Scalars['Boolean'];
+  options: Array<Scalars['String']>;
+  question: Scalars['String'];
+};
+
+export type SelectQuestionInput = {
+  multiSelect: Scalars['Boolean'];
+  options: Array<Scalars['String']>;
+  question: Scalars['String'];
+};
+
+export type TextQuestion = Question & {
+  __typename?: 'TextQuestion';
+  _id: Scalars['ID'];
+  question: Scalars['String'];
+};
+
+export type TextQuestionInput = {
+  question: Scalars['String'];
+};
+
+export type CreateFormMutationVariables = Exact<{ [key: string]: never; }>;
+
+
+export type CreateFormMutation = { __typename?: 'Mutation', createForm?: { __typename?: 'Questionnaire', _id: string, title: string } | null };
+
+export type CreateQuestionMutationVariables = Exact<{
+  formId: Scalars['ID'];
+}>;
+
+
+export type CreateQuestionMutation = { __typename?: 'Mutation', q1?: { __typename: 'SelectQuestion' } | { __typename: 'TextQuestion' } | null, q2?: { __typename: 'SelectQuestion' } | { __typename: 'TextQuestion' } | null };
+
+export type GetQuestionnaireQueryVariables = Exact<{
+  formId: Scalars['ID'];
+}>;
+
+
+export type GetQuestionnaireQuery = { __typename?: 'Query', questionnaire?: { __typename?: 'Questionnaire', _id: string, title: string, questions: Array<{ __typename: 'SelectQuestion', _id: string, question: string, options: Array<string>, multiSelect: boolean } | { __typename: 'TextQuestion', _id: string, question: string }> } | null };
+
+
+export const CreateFormDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateForm"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createForm"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"form"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"title"},"value":{"kind":"StringValue","value":"form-1","block":false}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"_id"}},{"kind":"Field","name":{"kind":"Name","value":"title"}}]}}]}}]} as unknown as DocumentNode<CreateFormMutation, CreateFormMutationVariables>;
+export const CreateQuestionDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateQuestion"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"formId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"ID"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","alias":{"kind":"Name","value":"q1"},"name":{"kind":"Name","value":"createQuestion"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"formId"},"value":{"kind":"Variable","name":{"kind":"Name","value":"formId"}}},{"kind":"Argument","name":{"kind":"Name","value":"question"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"select"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"question"},"value":{"kind":"StringValue","value":"Do you like GraphQL?","block":false}},{"kind":"ObjectField","name":{"kind":"Name","value":"options"},"value":{"kind":"ListValue","values":[{"kind":"StringValue","value":"yes","block":false},{"kind":"StringValue","value":"no","block":false}]}},{"kind":"ObjectField","name":{"kind":"Name","value":"multiSelect"},"value":{"kind":"BooleanValue","value":false}}]}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","alias":{"kind":"Name","value":"q2"},"name":{"kind":"Name","value":"createQuestion"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"formId"},"value":{"kind":"Variable","name":{"kind":"Name","value":"formId"}}},{"kind":"Argument","name":{"kind":"Name","value":"question"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"text"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"question"},"value":{"kind":"StringValue","value":"What do you like about GraphQL?","block":false}}]}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}}]}}]} as unknown as DocumentNode<CreateQuestionMutation, CreateQuestionMutationVariables>;
+export const GetQuestionnaireDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetQuestionnaire"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"formId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"ID"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"questionnaire"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"id"},"value":{"kind":"Variable","name":{"kind":"Name","value":"formId"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"_id"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"questions"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"__typename"}},{"kind":"InlineFragment","typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"TextQuestion"}},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"_id"}},{"kind":"Field","name":{"kind":"Name","value":"question"}}]}},{"kind":"InlineFragment","typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"SelectQuestion"}},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"_id"}},{"kind":"Field","name":{"kind":"Name","value":"question"}},{"kind":"Field","name":{"kind":"Name","value":"options"}},{"kind":"Field","name":{"kind":"Name","value":"multiSelect"}}]}}]}}]}}]}}]} as unknown as DocumentNode<GetQuestionnaireQuery, GetQuestionnaireQueryVariables>;
