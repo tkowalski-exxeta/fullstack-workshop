f090de6a84cc4d71151d1c0f538a33104a4612fb
Thomas Kowalski <thomas.kowalski@exxeta.com>
2023-10-26 18:13:34 +0200

    Update dependencies

--- packages/server/src/resolver.ts
+++ packages/server/src/resolver.ts
@@ -1,13 +1,13 @@
-import { ObjectId } from "mongodb"
-import { GqlContext } from "./create-context"
-import { QuestionDB } from "./db/types"
+import { ObjectId } from "mongodb";
+import { GqlContext } from "./create-context";
+import { QuestionDB } from "./db/types";
 
 type FormInput = {
-  title: string
-}
+  title: string;
+};
 type FormArgs = {
-  form: FormInput
-}
+  form: FormInput;
+};
 export const resolvers = {
   Mutation: {
     createForm: async (_root, { form }: FormArgs, { db }: GqlContext) => {
@@ -15,57 +15,57 @@ export const resolvers = {
         _id: new ObjectId(),
         title: form.title,
         questions: [],
-      }
-      await db.forms.insertOne(formDB)
-      return formDB
+      };
+      await db.forms.insertOne(formDB);
+      return formDB;
     },
     createQuestion: async (_root, { formId, question }, { db }: GqlContext) => {
-      const input = question.select ?? question.text
-      const type = !!question.select ? "SelectQuestion" : "TextQuestion"
+      const input = question.select ?? question.text;
+      const type = !!question.select ? "SelectQuestion" : "TextQuestion";
       const questionCreated: QuestionDB = {
         _id: new ObjectId(),
         questionType: type,
         ...input,
-      }
+      };
       const res = await db.forms.updateOne(
         { _id: new ObjectId(formId) },
         { $push: { questions: questionCreated } }
-      )
-      return res.matchedCount === 1 ? questionCreated : null
+      );
+      return res.matchedCount === 1 ? questionCreated : null;
     },
     updateForm: async (_root, { formId, form }, { db }: GqlContext) => {
-      await db.forms.updateOne({ _id: new ObjectId(formId) }, { $set: form })
+      await db.forms.updateOne({ _id: new ObjectId(formId) }, { $set: form });
       const formUpdated = await db.forms.findOne({
         _id: new ObjectId(formId),
-      })
-      return formUpdated
+      });
+      return formUpdated;
     },
     updateQuestion: async (
       _root,
       { formId, questionId, question },
       { db }: GqlContext
     ) => {
-      const input = question.select ?? question.text
-      const type = !!question.select ? "SelectQuestion" : "TextQuestion"
+      const input = question.select ?? question.text;
+      const type = !!question.select ? "SelectQuestion" : "TextQuestion";
       const questionUpdated = {
         _id: new ObjectId(questionId),
         questionType: type,
         ...input,
-      }
+      };
       const result = await db.forms.updateOne(
         {
           _id: new ObjectId(formId),
           "questions._id": new ObjectId(questionId),
         },
         { $set: { "questions.$": questionUpdated } }
-      )
-      return result.matchedCount === 1 ? questionUpdated : null
+      );
+      return result.matchedCount === 1 ? questionUpdated : null;
     },
     deleteForm: async (_root, { formId }, { db }: GqlContext) => {
       const result = await db.forms.deleteOne({
         _id: new ObjectId(formId),
-      })
-      return result.deletedCount === 1
+      });
+      return result.deletedCount === 1;
     },
     deleteQuestion: async (
       _root,
@@ -75,28 +75,32 @@ export const resolvers = {
       const result = await db.forms.updateOne(
         { _id: new ObjectId(formId) },
         { $pull: { questions: { _id: new ObjectId(questionId) } } }
-      )
-      return result.modifiedCount === 1
+      );
+      return result.modifiedCount === 1;
     },
   },
   Query: {
     forms: async (_root, _args, { db }: GqlContext) => {
-      return db.forms.find().toArray()
+      return db.forms.find().toArray();
     },
     formById: async (_root, { id }, { db }: GqlContext) => {
-      return db.forms.findOne({ _id: new ObjectId(id) })
+      return db.forms.findOne({ _id: new ObjectId(id) });
     },
   },
   Question: {
-    __resolveType: (question) => {
-      switch (question.questionType) {
-        case "SelectQuestion":
-          return "SelectQuestion"
-        case "TextQuestion":
-          return "TextQuestion"
+    __resolveType: (question: QuestionDB) => {
+      switch (question.type) {
+        case "select":
+          return "SelectQuestion";
+        case "text":
+          return "TextQuestion";
         default:
-          throw Error(`Question type ${question.questionType} is invalid.`)
+          invalidQuestionType(question);
       }
     },
   },
+};
+
+function invalidQuestionType(question: never): never {
+  throw Error(`Invalid question type: ${question}`);
 }
