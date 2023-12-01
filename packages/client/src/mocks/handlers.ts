09900c547fe7c9db2f9b916580d55d27dbd42b9f
Thomas Kowalski <thomas.kowalski@exxeta.com>
2023-12-01 11:22:09 +0100

    add typename to mock-data

--- packages/client/src/mocks/handlers.ts
+++ packages/client/src/mocks/handlers.ts
@@ -3,10 +3,12 @@ import { HttpResponse, graphql } from "msw";
 // Mock Data
 export const mockForms = [
   {
+    __typename: "Form",
     _id: "6479b7e2762f4f829cb7c316",
     title: "Test Form",
   },
   {
+    __typename: "Form",
     _id: "648c5d0b7c4bc517ee1bcd9b",
     title: "Untitled Form",
   },
@@ -15,14 +17,17 @@ const backend = graphql.link("http://localhost:4000/graphql");
 
 // Define handlers that catch the corresponding requests and returns the mock data.
 export const handlers = [
-  backend.operation((args) => {
-    console.log("Intercepted a GraphQL operation:", args);
-    return HttpResponse.json({ data: { forms: mockForms } });
-  }),
+  // backend.operation((args) => {
+  //   console.log("Intercepted a GraphQL operation:", args);
+  //   return HttpResponse.json({ data: { forms: mockForms } });
+  // }),
   backend.query("FormListPage", ({ query }) => {
-    console.log('Intercepted a "FormListPage" GraphQL query:', query);
+    // console.log('Intercepted a "FormListPage" GraphQL query:', query);
     return HttpResponse.json({
-      data: { forms: mockForms },
+      data: {
+        __typename: "Query",
+        forms: mockForms,
+      },
     });
   }),
 ];
